721153e6f90cfc57502ab212d5ba5d225068c9f6
xfelippi-ship-it <xfelippi@gmail.com>
2026-04-08 17:35:54 -0300

    fix: remove dynamic ssr:false do ProductGrid

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import dynamic from 'next/dynamic'
 import Header from '@/components/store/Header'
 import HeroBanner from '@/components/store/HeroBanner'
 import TrustBar from '@/components/store/TrustBar'
 import PromoBanner from '@/components/store/PromoBanner'
 import Footer from '@/components/store/Footer'
-
-const ProductGrid = dynamic(() => import('@/components/store/ProductGrid'), { ssr: false })
+import ProductGrid from '@/components/store/ProductGrid'
 
 export default function Home() {
   return (
