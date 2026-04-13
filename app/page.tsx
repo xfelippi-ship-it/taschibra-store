d9ed5319c3dff6d0a5ec57b4433da7b06fd6c6e7
xfelippi-ship-it <xfelippi@gmail.com>
2026-04-13 12:08:41 -0300

    debug: remover PromoBanner e segundo ProductGrid temporariamente

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import { Suspense } from 'react'
 import Header from '@/components/store/Header'
 import HeroBanner from '@/components/store/HeroBanner'
 import TrustBar from '@/components/store/TrustBar'
-import PromoBanner from '@/components/store/PromoBanner'
 import Footer from '@/components/store/Footer'
 import ProductGrid from '@/components/store/ProductGrid'
 
@@ -14,10 +13,6 @@ export default function Home() {
       <TrustBar />
       <Suspense fallback={<div className="h-96" />}>
         <ProductGrid title="Lançamentos" categorySlug="lancamentos" limit={8} />
-      </Suspense>
-      <PromoBanner />
-      <Suspense fallback={<div className="h-96" />}>
-        <ProductGrid title="Mais Vendidos" categorySlug="outlet" limit={8} />
       </Suspense>
       <Footer />
     </main>
