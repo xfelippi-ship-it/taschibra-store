e4a0f43bc0e5e57ef983cff71711ce39dfb2064e
xfelippi-ship-it <xfelippi@gmail.com>
2026-04-13 12:10:12 -0300

    debug: testar home sem ProductGrid

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import { Suspense } from 'react'
 import Header from '@/components/store/Header'
 import HeroBanner from '@/components/store/HeroBanner'
 import TrustBar from '@/components/store/TrustBar'
 import Footer from '@/components/store/Footer'
-import ProductGrid from '@/components/store/ProductGrid'
 
 export default function Home() {
   return (
@@ -11,9 +9,6 @@ export default function Home() {
       <Header />
       <HeroBanner />
       <TrustBar />
-      <Suspense fallback={<div className="h-96" />}>
-        <ProductGrid title="Lançamentos" categorySlug="lancamentos" limit={8} />
-      </Suspense>
       <Footer />
     </main>
   )
